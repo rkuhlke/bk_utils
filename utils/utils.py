490bf4de030f6196ab1ad50be03725e5a2904344
Robert Kuhlke <bkuhlke@yahoo.com>
2021-12-07 21:42:57 -0500

    adding send2Telegram method that sends messages to telegram chat platform

---
+++ utils/utils.py
@@ -0,0 +1,12 @@
+import requests
+
+class Utils:
+    def send2Telegram(bot_id, group, text):
+        """
+        sends the message to telegram
+        :param group: choose what telegram group to send message to
+        :param text: allows you to choose what message to send
+        :return: message
+        """
+        # sends a message to telegram
+        return requests.get(f"https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={group}=&text={text}")
